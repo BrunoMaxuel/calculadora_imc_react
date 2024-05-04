7620781d298944fa90eef6a590244a0145b3c7d3
bruno maxuel <brunomaxuel2@gmail.com>
2024-05-04 12:23:19 -0300

    envio ao repositorio

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
+import paweredImg from './assets/powered.png';
 
 function App() {
+  const [heightfield, setHeightField] = useState<number>(0);
+  const [weightfield, setWeightField] = useState<number>(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.main}>
+        <header>
+            <div className={styles.headerContainer}>
+                <img src={paweredImg} alt="" width={150}/>
+            </div>
+        </header>
+        <div className={styles.container}>
+            <div className={styles.leftSide}>
+                <h1>Calcule o seu imc</h1>
+                <p>Imc é o indice de massa corporal</p>
+                <input 
+                  type="number" placeholder="Digite sua altura em metros Ex: 1.5" 
+                  value={heightfield > 0 ? heightfield : ''}
+                />
+            </div>
+            <div className={styles.rightSide}>
+              conteudoright
+            </div>
+        </div>
     </div>
   );
 }
