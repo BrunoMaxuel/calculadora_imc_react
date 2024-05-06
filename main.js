c9def7e57e03943169beb8fda64cad09024b311f
bruno maxuel <brunomaxuel2@gmail.com>
2024-05-06 09:16:38 -0300

    sistema com electron compilando

--- main.js
+++ main.js
@@ -7,18 +7,14 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1300,
     height: 700,
+    icon: './build/favicon.ico',
     autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   });
-  mainWindow.maximize();
-  mainWindow.loadURL('http://localhost:3000');
-  // mainWindow.loadFile(path.join(__dirname, 'public', 'index.html'));
-}
-
-function closeWindow() {
-  app.quit(); 
+  // mainWindow.loadURL('http://localhost:3000');
+  mainWindow.loadFile('./build/index.html');
 }
 
 app.whenReady().then(() => {
